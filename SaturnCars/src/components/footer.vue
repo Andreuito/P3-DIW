05c935ac3696f2ca0419366f0190bfc89178f294
Andreu S.G <asallerasgaitan@cifpfbmoll.eu>
2023-01-26 21:07:42 +0100

    footer

--- SaturnCars/src/components/footer.vue
+++ SaturnCars/src/components/footer.vue
@@ -4,9 +4,9 @@
     <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
         <div class="xl:grid xl:grid-cols-3 xl:gap-8">
         <div class="space-y-8 xl:col-span-1">
-            <img class="h-10" src="../assets/sliderLogo.png" alt="Company name">
+            <img class="h-30" src="img/logo-white2.png" alt="Company name">
             <p class="text-gray-500 text-base">
-            Creating a slider for fun and educational purposes using Vue Js & Tailwind
+            SaturnCars is a car dealership that sells new and used cars. We also offer financing, service, and parts.
             </p>
         </div>
        </div>
