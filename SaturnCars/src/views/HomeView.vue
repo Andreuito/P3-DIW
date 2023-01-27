8c222102799f421f71f062a56b1a3b785eb70e4d
Andreu S.G <asallerasgaitan@cifpfbmoll.eu>
2023-01-27 19:18:31 +0100

    images, and carousel

--- SaturnCars/src/views/HomeView.vue
+++ SaturnCars/src/views/HomeView.vue
@@ -29,18 +29,60 @@ export default {
 
 <template>
     <!-- slider -->
-    <swiper :modules="modules" :slides-per-view="1" :autoplay="true" :space-between="50" :navigation="true" @swiper="onSwiper" @slideChange="onSlideChange">
+    <swiper :modules="modules" :slides-per-view="1" :autoplay="true" :space-between="50" :navigation="true"
+        @swiper="onSwiper" @slideChange="onSlideChange">
         <swiper-slide> <img class="rounded-t-lg" src="img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" /></swiper-slide>
-        <swiper-slide> <img class="rounded-t-lg" src="img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" /></swiper-slide>
-        <swiper-slide> <img class="rounded-t-lg" src="img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" /></swiper-slide>
-        
+        <swiper-slide> <img class="rounded-t-lg" src="img/hakon-sataoen-qyfco1nfMtg-unsplash.jpg"
+                alt="" /></swiper-slide>
+        <swiper-slide> <img class="rounded-t-lg" src="img/olav-tvedt-6lSBynPRaAQ-unsplash.jpg" alt="" /></swiper-slide>
     </swiper>
     <!-- cards -->
     <div class="flex justify-center">
+        <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="" />
+            <div class="p-6 flex flex-col justify-start">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                <p class="text-gray-700 text-base mb-4">
+                    This is a wider card with supporting text below as a natural lead-in to additional content. This
+                    content is a little bit longer.
+                </p>
+                <button type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+            </div>
+        </div>
+        <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="" />
+            <div class="p-6 flex flex-col justify-start">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                <p class="text-gray-700 text-base mb-4">
+                    This is a wider card with supporting text below as a natural lead-in to additional content. This
+                    content is a little bit longer.
+                </p>
+                <button type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+            </div>
+        </div>
+        <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="" />
+            <div class="p-6 flex flex-col justify-start">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                <p class="text-gray-700 text-base mb-4">
+                    This is a wider card with supporting text below as a natural lead-in to additional content. This
+                    content is a little bit longer.
+                </p>
+                <button type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center p-10">
         <div class="rounded-lg shadow-lg bg-white max-w-sm">
-            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img class="rounded-t-lg" src="img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
-            </a>
+            <div class="w-full h-64 overflow-hidden">
+                <img class="rounded-t-lg" src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="" />
+            </div>
             <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
                 <p class="text-gray-700 text-base mb-4">
@@ -52,9 +94,9 @@ export default {
             </div>
         </div>
         <div class="rounded-lg shadow-lg bg-white max-w-sm">
-            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img class="rounded-t-lg" src="img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
-            </a>
+            <div class="w-full h-64 overflow-hidden">
+                <img class="rounded-t-lg" src="img/pexels-pixabay-164634.jpg" alt="" />
+            </div>
             <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
                 <p class="text-gray-700 text-base mb-4">
@@ -66,9 +108,9 @@ export default {
             </div>
         </div>
         <div class="rounded-lg shadow-lg bg-white max-w-sm">
-            <a href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-                <img class="rounded-t-lg" src="img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
-            </a>
+            <div class="w-full h-64 overflow-hidden">
+                <img class="rounded-t-lg" src="img/pexels-pixabay-164634.jpg" alt="" />
+            </div>
             <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
                 <p class="text-gray-700 text-base mb-4">
@@ -80,4 +122,12 @@ export default {
             </div>
         </div>
     </div>
+    <!--slider-->
+    <swiper :modules="modules" :slides-per-view="1" :autoplay="true" :space-between="50" :navigation="true"
+        @swiper="onSwiper" @slideChange="onSlideChange">
+        <swiper-slide> <img class="rounded-t-lg" src="img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" /></swiper-slide>
+        <swiper-slide> <img class="rounded-t-lg" src="img/hakon-sataoen-qyfco1nfMtg-unsplash.jpg"
+                alt="" /></swiper-slide>
+        <swiper-slide> <img class="rounded-t-lg" src="img/olav-tvedt-6lSBynPRaAQ-unsplash.jpg" alt="" /></swiper-slide>
+    </swiper>
 </template>
