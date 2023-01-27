0e5ed66d6d720ec73d4447e61a174cafbbc127df
Andreu S.G <asallerasgaitan@cifpfbmoll.eu>
2023-01-27 16:33:58 +0100

    adding cards and images

--- SaturnCars/src/components/footer.vue
+++ SaturnCars/src/components/footer.vue
@@ -1,15 +1,26 @@
 <template>
-        <footer class="bg-white" aria-labelledby="footerHeading">
-    <h2 id="footerHeading" class="sr-only">Footer</h2>
-    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-        <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-        <div class="space-y-8 xl:col-span-1">
-            <img class="h-30" src="img/logo-white2.png" alt="Company name">
-            <p class="text-gray-500 text-base">
-            SaturnCars is a car dealership that sells new and used cars. We also offer financing, service, and parts.
-            </p>
+    <footer class="bg-white" aria-labelledby="footerHeading">
+        <div class="relative overflow-hidden bg-no-repeat bg-cover" style="
+    background-position: 50%;
+    background-image: url('img/hakon-sataoen-qyfco1nfMtg-unsplash.jpg');
+    height: 350px;
+  ">
+            <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed"
+                style="background-color: rgba(0, 0, 0, 0.75)">
+                <h2 id="footerHeading" class="sr-only">Footer</h2>
+                <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+                    <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+                        <div class="space-y-8 xl:col-span-1">
+                            <img class="h-30" src="img/logo-white2.png" alt="Company name">
+                            <p class="text-gray-500 text-base">
+                                SaturnCars is a car dealership that sells new and used cars. We also offer financing,
+                                service,
+                                and parts.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-       </div>
-    </div>
     </footer>
 </template>
