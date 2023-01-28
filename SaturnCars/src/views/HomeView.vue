fb03d7fc61bad1322ab600fe3cc707277d5cd1f9
Andreu S.G <asallerasgaitan@cifpfbmoll.eu>
2023-01-28 19:13:36 +0100

    new footer

--- SaturnCars/src/views/HomeView.vue
+++ SaturnCars/src/views/HomeView.vue
@@ -2,12 +2,13 @@
 
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Navigation, Autoplay, Scrollbar } from 'swiper';
+import { Navigation, Pagination, Autoplay, Scrollbar } from 'swiper';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
-import 'swiper/css/scrollbar'
+import 'swiper/css/scrollbar';
+import 'swiper/css/pagination';
 
 export default {
     components: {
@@ -22,7 +23,7 @@ export default {
             console.log('slide change');
         };
         return {
-            modules: [Navigation, Autoplay, Scrollbar],
+            modules: [Navigation, Pagination, Autoplay, Scrollbar],
             onSwiper,
             onSlideChange,
         };
@@ -34,50 +35,30 @@ export default {
     <!-- slider -->
     <swiper :modules="modules" :slides-per-view="1" :autoplay="true" :navigation="true" :loop="true"
         :scrollbar="true" @swiper="onSwiper" @slideChange="onSlideChange">
-        <swiper-slide><img class="" src="img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" /></swiper-slide>
+        <swiper-slide>
+            <div>
+                <img class="" src="img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
+            </div>
+        </swiper-slide>
         <swiper-slide> <img class="" src="img/hakon-sataoen-qyfco1nfMtg-unsplash-edited2.jpg"
                 alt="" /></swiper-slide>
         <swiper-slide> <img class="" src="img/olav-tvedt-6lSBynPRaAQ-unsplash.jpg" alt="" /></swiper-slide>
     </swiper>
     <!-- cards -->
-    <div class="flex justify-center">
-        <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-                src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="" />
-            <div class="p-6 flex flex-col justify-start">
-                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-                <p class="text-gray-700 text-base mb-4">
-                    This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer.
-                </p>
-                <button type="button"
-                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
-            </div>
-        </div>
-        <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-                src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="" />
-            <div class="p-6 flex flex-col justify-start">
-                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-                <p class="text-gray-700 text-base mb-4">
-                    This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer.
-                </p>
-                <button type="button"
-                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
-            </div>
-        </div>
-        <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-                src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="" />
-            <div class="p-6 flex flex-col justify-start">
-                <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
-                <p class="text-gray-700 text-base mb-4">
-                    This is a wider card with supporting text below as a natural lead-in to additional content. This
-                    content is a little bit longer.
-                </p>
-                <button type="button"
-                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+    <div class="flex m-40 space-y-20">
+        <div class="flex justify-center">
+            <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+                <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                    src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="" />
+                <div class="p-6 flex flex-col justify-start">
+                    <h5 class="text-gray-900 text-xl font-medium mb-2">Card title</h5>
+                    <p class="text-gray-700 text-base mb-4">
+                        This is a wider card with supporting text below as a natural lead-in to additional content. This
+                        content is a little bit longer.
+                    </p>
+                    <button type="button"
+                        class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+                </div>
             </div>
         </div>
     </div>
