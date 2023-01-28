15ddaf5ce09417d61320836f036cf559f0113977
Andreu S.G <asallerasgaitan@cifpfbmoll.eu>
2023-01-28 21:06:57 +0100

    new cards release

--- SaturnCars/src/views/HomeView.vue
+++ SaturnCars/src/views/HomeView.vue
@@ -42,7 +42,33 @@ export default {
         <swiper-slide> <img class="" src="img/olav-tvedt-6lSBynPRaAQ-unsplash.jpg" alt="imageCar" /></swiper-slide>
     </swiper>
     <!-- cards -->
-    <div class="grid-rows-6 gap-10 m-28 space-y-20">
+    <!--    <div class="grid-cols-3 gap-2 m-28 space-y-10">
+        <div class="flex flex-col-reverse md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="imageCar" />
+            <div class="p-6 flex flex-col justify-start">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Saturn StarOne</h5>
+                <p class="text-gray-700 text-base mb-4">
+                    This is a wider card with supporting text below as a natural lead-in to additional content. This
+                    content is a little bit longer.
+                </p>
+                <button type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+            </div>
+        </div>
+        <div class="flex flex-col-reverse md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+                src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="imageCar" />
+            <div class="p-6 flex flex-col justify-start">
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Saturn StarOne</h5>
+                <p class="text-gray-700 text-base mb-4">
+                    This is a wider card with supporting text below as a natural lead-in to additional content. This
+                    content is a little bit longer.
+                </p>
+                <button type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
+            </div>
+        </div>
         <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
             <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
                 src="img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="imageCar" />
@@ -68,10 +94,94 @@ export default {
                 <button type="button"
                     class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
             </div>
+        </div>
+    </div>  -->
+    <div class="grid grid-cols-3 gap-2">
+        <div class="col-span-2 flex flex-col justify-between">
+            <h2 class="text-xl font-semibold">
+                How to Fetch API in Vue JS
+            </h2>
+            <p class="text-gray-500 text-base">
+                This is a wider card with supporting text below as a natural lead-in to additional content. This
+                content is a little bit longer.
+            </p>
+        </div>
+        <div class="col-span-1">
+            <img class="" src="img/logoRemaSteredB.PNG">
+        </div>
+    </div>
+    <div class="grid grid-cols-3 gap-2">
+        <div class="col-span-1">
+                <img class="" src="img/logoRemaSteredB.PNG">
+            </div>
+        <div class="col-span-2 flex flex-col justify-between">
+            <h2 class="text-xl font-semibold">
+                How to Fetch API in Vue JS
+            </h2>
+            <p class="text-gray-500 text-base">
+                This is a wider card with supporting text below as a natural lead-in to additional content. This
+                content is a little bit longer.
+            </p>
+        </div>
+    </div>
+    <div class="grid grid-cols-3 gap-2">
+        <div class="col-span-2 flex flex-col justify-between">
+            <h2 class="text-xl font-semibold">
+                How to Fetch API in Vue JS
+            </h2>
+            <p class="text-gray-500 text-base">
+                This is a wider card with supporting text below as a natural lead-in to additional content. This
+                content is a little bit longer.
+            </p>
+        </div>
+        <div class="col-span-1">
+            <img class="" src="img/logoRemaSteredB.PNG">
+        </div>
+    </div>
+    <div class="grid grid-cols-3 gap-2">
+        <div class="col-span-1">
+                <img class="" src="img/logoRemaSteredB.PNG">
+            </div>
+        <div class="col-span-2 flex flex-col justify-between">
+            <h2 class="text-xl font-semibold">
+                How to Fetch API in Vue JS
+            </h2>
+            <p class="text-gray-500 text-base">
+                This is a wider card with supporting text below as a natural lead-in to additional content. This
+                content is a little bit longer.
+            </p>
+        </div>
+    </div>
+    <div class="grid grid-cols-3 gap-2">
+        <div class="col-span-2 flex flex-col justify-between">
+            <h2 class="text-xl font-semibold">
+                How to Fetch API in Vue JS
+            </h2>
+            <p class="text-gray-500 text-base">
+                This is a wider card with supporting text below as a natural lead-in to additional content. This
+                content is a little bit longer.
+            </p>
+        </div>
+        <div class="col-span-1">
+            <img class="" src="img/logoRemaSteredB.PNG">
+        </div>
+    </div>
+    <div class="grid grid-cols-3 gap-2">
+        <div class="col-span-1">
+                <img class="" src="img/logoRemaSteredB.PNG">
+            </div>
+        <div class="col-span-2 flex flex-col justify-between">
+            <h2 class="text-xl font-semibold">
+                How to Fetch API in Vue JS
+            </h2>
+            <p class="text-gray-500 text-base">
+                This is a wider card with supporting text below as a natural lead-in to additional content. This
+                content is a little bit longer.
+            </p>
         </div>
     </div>
     <!--Multiple slider-->
-    <swiper :modules="modules" :slides-per-view="3" :autoplay="true" :navigation="true" :loop="true" :scrollbar="true"
+    <swiper :modules="modules" :slides-per-view="4" :autoplay="true" :navigation="true" :loop="true" :scrollbar="true"
         @swiper="onSwiper" @slideChange="onSlideChange">
         <swiper-slide> <img src="img/campbell-3ZUsNJhi_Ik-unsplash-edited.jpg" alt="" /></swiper-slide>
         <swiper-slide> <img src="img/hakon-sataoen-qyfco1nfMtg-unsplash-edited.jpg" alt="" /></swiper-slide>
