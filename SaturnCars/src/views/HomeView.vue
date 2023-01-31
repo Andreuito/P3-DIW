e713f705de19366eaf328f4cecaae4521cec51d0
Andreu S.G <asallerasgaitan@cifpfbmoll.eu>
2023-01-31 16:07:31 +0100

    changes

--- SaturnCars/src/views/HomeView.vue
+++ SaturnCars/src/views/HomeView.vue
@@ -62,12 +62,10 @@ export default {
         </h2>
     </div>
     <!-- cards -->
-    <div class="grid sm:grid-cols-2 gap-2 m-28 space-y-4 grid-cols-1">
+    <div class="grid sm:grid-cols-2 gap-2 m-28 grid-cols-1">
         <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
-                src="src/assets/img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="imageCar" />
             <div class="p-6 flex flex-col justify-start">
-                <h5 class="text-gray-900 text-xl font-medium mb-2">Saturn Star 6</h5>
+                <h5 class="text-gray-900 text-xl font-medium mb-2">Saturn Star 5</h5>
                 <p class="text-gray-700 text-base mb-4">
                     This is a wider card with supporting text below as a natural lead-in to additional content. This
                     content is a little bit longer.
@@ -75,6 +73,8 @@ export default {
                 <button type="button"
                     class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
             </div>
+            <img class=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-r-lg"
+                src="src/assets/img/ville-kaisla-HNCSCpWrVJA-unsplash-edited.jpg" alt="imageCar" />
         </div>
         <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
             <div class="p-6 flex flex-col justify-start">
