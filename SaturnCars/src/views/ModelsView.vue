674815165a71f3e669012c19f61c8e88623cdd11
Andreu S.G <asallerasgaitan@cifpfbmoll.eu>
2023-02-11 16:35:32 +0100

    css propi, db omplida, retocs generals

--- SaturnCars/src/views/ModelsView.vue
+++ SaturnCars/src/views/ModelsView.vue
@@ -1,104 +1,86 @@
 <script>
-// Import Swiper Vue.js components
-import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Navigation, Pagination, Autoplay, Scrollbar } from 'swiper';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/autoplay';
-import 'swiper/css/scrollbar';
-import 'swiper/css/pagination';
-
-export default {
-    components: {
-        Swiper,
-        SwiperSlide,
-    },
-    setup() {
-        const onSwiper = (swiper) => {
-            console.log(swiper);
-        };
-        const onSlideChange = () => {
-            console.log('slide change');
-        };
-        return {
-            modules: [Navigation, Pagination, Autoplay, Scrollbar],
-            onSwiper,
-            onSlideChange,
-        };
-    },
-}; 
 </script>
 <template>
+    <!--title-->
+    <div class="flex justify-center mb-24">
+        <h1 class="flex justify-center text-7xl p-10 font-bold text-gray-800">Our models</h1>
+    </div>
+
     <div class="grid grid-cols-1 sm:grid-cols-2 mb-10 mt-10 ml-10">
         <img class="rounded-lg" src="src/assets/img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
         <div class="flex mb-24">
-            <h1 class="flex text-7xl p-10 font-bold text-gray-800">
-                RING 1.
+            <h1 class="flex text-6xl p-10 font-bold text-gray-800">
+                Model Ring 1.
             </h1>
             <p>
-                
+                Some quick example text to build on the card title and make up the bulk of the card's
+                content.
             </p>
         </div>
     </div>
     <div class="grid grid-cols-1 sm:grid-cols-2 mb-10 mr-10">
         <div class="flex mb-24">
-            <h1 class="flex text-7xl p-10 font-bold text-gray-800">
-                RING 2.
+            <h1 class="flex text-6xl p-10 font-bold text-gray-800">
+                Model Ring 2.
             </h1>
             <p>
-                
+
             </p>
         </div>
         <img class="rounded-lg" src="src/assets/img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 mb-10">
-        <img class="rounded-lg" src="src/assets/img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
+    <div class="grid grid-cols-1 sm:grid-cols-2 mb-10 ml-10">
+        <img class="rounded-lg hover:scale-110 transition duration-300 ease-in-out" src="src/assets/img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
+        <div class="flex flex-col mb-24 ml-10">
+            <h1 class="flex text-6xl p-10 font-bold text-gray-800">
+                Model Ring 3.
+            </h1>
+            <p class="flex text-gray-700 text-base mb-4 p-6">
+                Some quick example text to build on the card title and make up the bulk of the card's
+                content.
+            </p>
+            <div class="flex justify-center">
+                <button class="button"> 
+                    Buy me!
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 mb-10 mr-10">
         <div class="flex mb-24">
-            <h1 class="flex text-7xl p-10 font-bold text-gray-800">
-                RING 3.
+            <h1 class="flex text-6xl p-10 font-bold text-gray-800">
+                Model Ring 4.
             </h1>
             <p>
-                
+
+            </p>
+        </div>
+        <img class="rounded-lg" src="src/assets/img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 mb-10 ml-10">
+        <img class="rounded-lg" src="src/assets/img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
+        <div class="flex mb-24">
+            <h1 class="flex text-6xl p-10 font-bold text-gray-800">
+                Model Ring 5.
+            </h1>
+            <p>
+
             </p>
         </div>
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 mb-10">
+    <div class="grid grid-cols-1 sm:grid-cols-2 mb-10 mr-10">
         <div class="flex mb-24">
-            <h1 class="flex text-7xl p-10 font-bold text-gray-800">
-                RING 4.
+            <h1 class="flex text-6xl p-10 font-bold text-gray-800">
+                Model Ring 6.
             </h1>
             <p>
-                
-            </p>
-        </div>
-        <img class="rounded-lg" src="src/assets/img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
-    </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 mb-10">
-        <img class="rounded-lg" src="src/assets/img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
-        <div class="flex mb-24">
-            <h1 class="flex text-7xl p-10 font-bold text-gray-800">
-                RING 5.
-            </h1>
-            <p>
-                
-            </p>
-        </div>
-    </div>
-    <div class="grid grid-cols-1 sm:grid-cols-2 mb-10">
-        <div class="flex mb-24">
-            <h1 class="flex text-7xl p-10 font-bold text-gray-800">
-                RING 6.
-            </h1>
-            <p>
-                
+
             </p>
         </div>
         <img class="rounded-lg" src="src/assets/img/campbell-3ZUsNJhi_Ik-unsplash.jpg" alt="" />
     </div>
 
     <div class="flex justify-center mt-64 mb-24">
-        <h1 class="flex justify-center text-7xl p-10 font-bold text-gray-800">Coming soon Ring 7.</h1>
+        <h1 class="flex justify-center text-7xl p-10 font-bold text-gray-800">Coming soon model Ring 7.</h1>
     </div>
 </template>
